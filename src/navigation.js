4d934c80aacb20964144b18d83891c74b12c8075
Thuzar <thuzar.ts92@gmail.com>
2024-08-08 13:40:03 +0700

    Layout and pages

--- src/navigation.js
+++ src/navigation.js
@@ -1,46 +1,55 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { getAsset, getPermalink } from './utils/permalinks';
 
 export const headerData = {
   links: [
     {
-      text: 'Homes',
+      text: 'Home',
+      href: '/',
+    },
+    {
+      text: 'Rooms',
       links: [
         {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
+          text: 'Superior',
+          href: getPermalink('/superior'),
         },
         {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
+          text: 'Deluxe',
+          href: getPermalink('/deluxe'),
         },
         {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
+          text: 'Royal Suite',
+          href: getPermalink('/royal-suite'),
         },
         {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
+          text: 'Royal Family',
+          href: getPermalink('/royal-family'),
         },
       ],
+    },
+    {
+      text: 'Facilities',
+      href: getPermalink('/facilities'),
+    },
+
+    {
+      text: 'About',
+      href: getPermalink('/about'),
     },
     {
       text: 'Pages',
       links: [
         {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
+          text: 'Special Offers',
           href: getPermalink('/about'),
+        },
+        {
+          text: 'Gallery',
+          href: getPermalink('/gallery'),
+        },
+        {
+          text: 'Reviews',
+          href: getPermalink('/review'),
         },
         {
           text: 'Contact',
@@ -55,64 +64,6 @@ export const headerData = {
           href: getPermalink('/privacy'),
         },
       ],
-    },
-    {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
-    },
-    {
-      text: 'Widgets',
-      href: '#',
     },
   ],
   actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
@@ -176,8 +127,5 @@ export const footerData = {
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
-  footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
-  `,
+  footNote: '',
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,44 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
 import typographyPlugin from '@tailwindcss/typography';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
-        primary: 'var(--aw-color-primary)',
-        secondary: 'var(--aw-color-secondary)',
+        primary: {
+          100: '#D1FAE5', // Lightest shade
+          200: '#A7F3D0',
+          300: '#6EE7B7',
+          400: '#34D399',
+          500: '#22C55E', // Default
+          600: '#16A34A',
+          700: '#15803D',
+          800: '#166534',
+          900: '#14532D', // Darkest shade
+        },
+        secondary: {
+          100: '#ffb3b3',
+          200: '#ff6666',
+          300: '#ff3333',
+          400: '#ff0000',
+          500: '#a90000cc',
+          600: '#990000',
+          700: '#800000',
+          800: '#660000',
+          900: '#330000',
+        },
         accent: 'var(--aw-color-accent)',
         default: 'var(--aw-color-text-default)',
         muted: 'var(--aw-color-text-muted)',
       },
+      // colors: {
+      //   primary: 'var(--aw-color-primary)',
+      //   secondary: 'var(--aw-color-secondary)',
+      //   accent: 'var(--aw-color-accent)',
+      //   default: 'var(--aw-color-text-default)',
+      //   muted: 'var(--aw-color-text-muted)',
+      // },
       fontFamily: {
         sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
         serif: ['var(--aw-font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
